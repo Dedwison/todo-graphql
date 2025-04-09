f7c869f6780ec41733681c8bf3af5bffcf45aa18
Dedwison <dedwison@gmail.com>
2025-04-09 12:06:41 -0400

    todo getAll

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,11 +1,25 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String)
+    @Query(() => String, {description: 'Retorna Hola Mundo', name: 'hello'})
     helloWorld(): string {
         return "Hola mundo";
+    }
+
+    @Query(() => Float, { name: 'randomNumber', description: 'retorna numero ramdom'})
+    getRamndomNumber(): number {
+        return Math.random() * 100
+    }
+
+    // randomFromZeroTo
+    @Query(() => Int, {name: 'randomFromZeroTo', description: 'From zero to argument TO (exclusive) and default value is 6'})
+    getRandomFromZeroTo(
+        @Args('to', { type: () => Int, nullable: true }) to: number = 6
+    ): number {
+        
+        return Math.floor(Math.random() * to);
     }
 
 
